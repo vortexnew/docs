242ae596422cfcd8134c32484a2be82479abf020
fnicefire <fnicefire@gmail.com>
2026-01-01 18:04:45 +0100

    Footer Modifications

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -146,7 +146,7 @@ const config = {
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} VortexNew. All rights reserved.`,
       },
       prism: {
         theme: prismThemes.github,
